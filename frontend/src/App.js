c6f5c77327f6fbdb8181e51e9bd8068975e1430d
godswilleo <godswilleo.user1@gmail.com>
2023-05-22 10:51:51 +0100

    sass set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,26 @@ import React from  "react"
 
 export default function App(){
     return (
-        <div Style="background-color:red" className="container">I test</div>
+        <div Style="background-color:red" className="container">
+        <div className="logon-box">
+            <div className="top-logon-box">I-Test CBT</div>
+            <div className="bottom-logon-box">
+                
+                <form className="logon-form">
+                    <input placeholder="Enter your email"></input>
+                    <label></label>
+                    <br />
+
+                    <input placeholder="Enter your password"></input>
+                    <label></label>
+                    <br />
+
+                    <button>Login</button>
+                </form>
+ 
+              
+            </div>
+        </div>    
+        </div>
     )
 } 
