b0897997a441751038357dcb80588588d793be7f
godswilleo <godswilleo.user1@gmail.com>
2023-05-22 16:14:22 +0100

    set color variable in sass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,40 @@
 import React from  "react"
 
 export default function App(){
+
+    const [firstscreen, setfirstscreen] = React.useState(true)
+
+    function userLogon(e){
+
+        e.preventDefault()
+
+        const email = "hi"
+        const password = "he"
+
+
+
+        if (email == "hi" && password == "he")
+        {
+            setfirstscreen((prevfirstscreen) => prevfirstscreen == false)
+        }
+    }
+
     return (
         <div Style="background-color:red" className="container">
         <div className="logon-box">
-            <div className="top-logon-box">I-Test CBT</div>
+            <div className="top-logon-box"><h2>I-Test CBT</h2></div>
             <div className="bottom-logon-box">
                 
                 <form className="logon-form">
-                    <input placeholder="Enter your email"></input>
+                    <input type="text" id="email" name="email" placeholder="Enter your email" className="logon-input"></input>
                     <label></label>
                     <br />
 
-                    <input placeholder="Enter your password"></input>
+                    <input type="password" id="password" name="password" placeholder="Enter your password" className="logon-input"></input>
                     <label></label>
                     <br />
 
-                    <button>Login</button>
+                    <button onClick={userLogon}>Login</button>
                 </form>
  
               
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
-import "./app.css"
+import "./css/styles.css"
 
 
 ReactDOM.render(<App />, document.getElementById("root"))
